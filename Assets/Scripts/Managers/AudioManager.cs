674e99fd9da594436603a46f55b27b468417771d
DanLinhHuynh-Niwashi <146794831+DanLinhHuynh-Niwashi@users.noreply.github.com>
2025-03-03 20:43:00 +0700

    Add game manager

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager _instance;
+    [Header("Audio Sources")]
+    [SerializeField] private AudioSource bgmSource;
+    [SerializeField] private AudioSource gameSongSource;
+    [SerializeField] private AudioSource sfxSource;
 
     [Header("Audio Setting")]
 
@@ -30,15 +34,52 @@ public class AudioManager : MonoBehaviour
         }
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void PlayBGM(AudioClip clip)
     {
-        
+        bgmSource.clip = clip;
+        bgmSource.loop = true;
+        bgmSource.Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StopBGM()
     {
-        
+        bgmSource.Stop();
     }
+
+    public void PlayGameSong(AudioClip clip)
+    {
+        if (clip == null) return;
+        gameSongSource.clip = clip;
+        gameSongSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        if (clip == null) return;
+        sfxSource.PlayOneShot(clip);
+    }
+
+    public void PauseGameSong()
+    {
+        gameSongSource.Pause();
+    }
+
+    public void ContinueGameSong()
+    {
+        gameSongSource.Play();
+    }
+
+    public void StopGameSong()
+    {
+        gameSongSource.Stop();
+    }
+
+    public float GetGameSongSecond()
+    {
+        return gameSongSource.time;
+    }
+
+    public void SetBGMVolume(float volume) => bgmSource.volume = volume;
+    public void SetGameSongVolume(float volume) => gameSongSource.volume = volume;
+    public void SetSFXVolume(float volume) => sfxSource.volume = volume;
 }
---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private SongInfoSO songInfo;
+    [SerializeField] private ConfigSO config;
+    [SerializeField] private FloatPublisherSO gameSongUpdatePublisherSO;
+    private int nextNoteIndex = 0;
+
+    [SerializeField] private float travelDuration;
+
+    private void CalculateTravelDuration()
+    {
+        travelDuration = (config.BeatsShownOnScreen / songInfo.BPM) * 60f;
+        Debug.Log("Travel Duration: " + travelDuration);
+    }
+
+    public void Load(SongInfoSO songInfo, ConfigSO config)
+    {
+        this.songInfo = songInfo;
+        this.config = config;
+    }    
+    private void Start()
+    {
+        CalculateTravelDuration();
+        AudioManager.Instance.PlayGameSong(songInfo.songClip);
+    }
+
+    private void Update()
+    {
+        float songPosInSeconds = AudioManager.Instance.GetGameSongSecond();
+        gameSongUpdatePublisherSO.RaiseEvent(songPosInSeconds);
+    }
+
+    public void SpawnNote(float songPosInSeconds)
+    {
+        while (nextNoteIndex < songInfo.noteTimings.Count &&
+               songInfo.noteTimings[nextNoteIndex] - travelDuration <= songPosInSeconds)
+        {
+            float noteSpawnTime = songInfo.noteTimings[nextNoteIndex] - travelDuration;
+            GameEvents.TriggerNoteSpawn(noteSpawnTime, travelDuration);
+            nextNoteIndex++;
+        }
+    }
+}
