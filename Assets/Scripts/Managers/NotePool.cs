0909761fb675cfcc95153e5c8da2941b2c1cd2b6
DanLinhHuynh-Niwashi <146794831+DanLinhHuynh-Niwashi@users.noreply.github.com>
2025-03-03 20:42:14 +0700

    Add note prefab and note pool

---
+++ Assets/Scripts/Managers/NotePool.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class NotePool : MonoBehaviour
+{
+    public GameObject notePrefab;
+    public Transform spawnPoint;
+    public Transform endPoint;
+    public int poolSize = 20;
+
+    private Queue<GameObject> notePool;
+    private Queue<GameObject> activeNotes;
+
+    private void Awake()
+    {
+        InitializePool();
+    }
+
+    private void OnEnable()
+    {
+        GameEvents.OnNoteSpawn += SpawnNote;
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.OnNoteSpawn -= SpawnNote;
+    }
+
+    private void InitializePool()
+    {
+        notePool = new Queue<GameObject>();
+        activeNotes = new Queue<GameObject>();
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject note = Instantiate(notePrefab, transform);
+            note.SetActive(false);
+            notePool.Enqueue(note);
+        }
+    }
+
+    private void SpawnNote(float spawnTime, float travelDuration)
+    {
+        if (notePool.Count == 0) return;
+
+        GameObject note = notePool.Dequeue();
+
+        note.SetActive(true);
+
+        Vector2 endPos = new Vector2(endPoint.position.x, Random.Range(-2, 2));
+        note.GetComponent<GameNote>().Initialize(spawnPoint.position, endPos, spawnTime, travelDuration);
+
+        activeNotes.Enqueue(note);
+    }
+
+    public void ReturnToPool(GameObject note)
+    {
+        note.SetActive(false);
+        notePool.Enqueue(note);
+        if (activeNotes.Peek() == note)
+        {
+            activeNotes.Dequeue();
+        }
+    }
+
+    
+    public void CheckClosestActiveNote()
+    {
+        
+        if (activeNotes.Count == 0) return;
+        GameObject activeNote = activeNotes.Peek();
+        GameNote active = activeNote.GetComponent<GameNote>();
+        if (active.GetHitResult() == GameNote.HitResult.MISS)
+        {
+            activeNotes.Dequeue();
+            CheckClosestActiveNote();
+        }
+        active.CheckHit();
+    }
+}
---
+++ Assets/Scripts/Units/GameNote.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameNote : MonoBehaviour
+{
+    [SerializeField]
+    private float startX;
+    private float startY;
+    private float endX;
+    private float endY;
+    private float spawnTime;
+    private float travelDuration;
+    private HitResult hitResult;
+    private SpriteRenderer spriteRenderer;
+
+    public enum HitResult
+    {
+        NONE,
+        PERFECT,
+        GOOD,
+        BAD,
+        MISS
+    }
+
+    [SerializeField] GameObjectPublisherSO NoteDisappearPublisher;
+
+    public void Initialize(Vector2 startPoint, Vector2 endPoint, float spawnTime, float travelDuration)
+    {
+        this.startX = startPoint.x;
+        this.startY = startPoint.y;
+        this.endX = endPoint.x;
+        this.endY = endPoint.y;
+        this.spawnTime = spawnTime;
+        this.travelDuration = travelDuration;
+
+        this.hitResult = HitResult.NONE;
+
+        if (spriteRenderer == null)
+            spriteRenderer = GetComponent<SpriteRenderer>();
+
+        Color color = spriteRenderer.color;
+        color.a = 1f;
+        spriteRenderer.color = color;
+
+        transform.position = new Vector3(startX, startY, 1);
+    }
+
+    private float timeDifference;
+    public void UpdateNotePosition(float songTime)
+    {
+        float progress = (songTime - spawnTime) / travelDuration;
+
+        float overshoot = (progress > 1f) ? (1f - progress) * 15f : 0f;
+
+        timeDifference = songTime - (spawnTime + travelDuration);
+        if (timeDifference > config.badThreshold)
+        {
+            hitResult = HitResult.MISS;
+            Debug.Log(hitResult.ToString().ToUpper() + "!");
+            NoteDisappearPublisher.RaiseEvent(this.gameObject);
+
+        }
+
+        transform.position = new Vector3(Mathf.Lerp(startX, endX + overshoot, progress), Mathf.Lerp(startY, endY, progress), transform.position.z);
+
+    }
+
+    [SerializeField] private ConfigSO config;
+
+    public void CheckHit()
+    {
+        if (hitResult != HitResult.NONE) return; // Prevent multiple hits
+
+        SetHitResult(timeDifference);
+
+        if (hitResult != HitResult.NONE)
+        {
+            Debug.Log(hitResult.ToString().ToUpper() + "!"); // Show "PERFECT!", "GOOD!", etc.
+            NoteDisappearPublisher.RaiseEvent(this.gameObject);
+        }
+
+    }
+
+    private void SetHitResult(float timeDifference)
+    {
+        if (Mathf.Abs(timeDifference) <= config.perfectThreshold) hitResult = HitResult.PERFECT;
+        else if (Mathf.Abs(timeDifference) <= config.goodThreshold) hitResult = HitResult.GOOD;
+        else if (Mathf.Abs(timeDifference) <= config.badThreshold) hitResult = HitResult.BAD;
+        else hitResult = HitResult.NONE;
+    }
+
+    public HitResult GetHitResult()
+    {
+        return hitResult;
+    }
+
+}
