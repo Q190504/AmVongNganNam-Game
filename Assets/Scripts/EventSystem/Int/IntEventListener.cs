96c8b354de9de07dde762649213cbbb2e54cb9e9
DanLinhHuynh-Niwashi <146794831+DanLinhHuynh-Niwashi@users.noreply.github.com>
2025-03-03 21:39:41 +0700

    Add int event system

---
+++ Assets/Scripts/EventSystem/Int/IntEventListener.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class IntEventListener : MonoBehaviour
+{
+    [SerializeField] private UnityEvent<int> EventResponse;
+    [SerializeField] private IntPublisherSO publisher;
+
+    private void OnEnable()
+    {
+        publisher.OnEventRaised += Respond;
+    }
+
+    private void OnDisable()
+    {
+        publisher.OnEventRaised -= Respond;
+    }
+
+    private void Respond(int value)
+    {
+        EventResponse?.Invoke(value);
+    }
+}
