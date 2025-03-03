f93329935d82ac466b9323c9f0e67e770bdd5e17
DanLinhHuynh-Niwashi <146794831+DanLinhHuynh-Niwashi@users.noreply.github.com>
2025-03-03 20:40:00 +0700

    Add event system for audio clip and game object

---
+++ Assets/Scripts/EventSystem/AudioClip/AudioClipPublisherSO.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "New Audio Pulisher", menuName = "Scriptable Objects/Events/Audio Publisher")]
+public class AudioClipPublisherSO : ScriptableObject
+{
+    public UnityAction<AudioClip> OnEventRaised;
+
+    public void RaiseEvent(AudioClip obj)
+    {
+        OnEventRaised?.Invoke(obj);
+    }
+}
