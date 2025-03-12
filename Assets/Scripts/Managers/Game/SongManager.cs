50efda32beee5cda6124aebf94298822bbfca037
DanLinhHuynh-Niwashi <146794831+DanLinhHuynh-Niwashi@users.noreply.github.com>
2025-03-12 13:39:08 +0700

    Add song loader from online database

---
+++ Assets/Scripts/Managers/Game/SongManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SongManager : MonoBehaviour
+{
+    public static SongManager Instance;
+    public List<SongInfoSO> songInfos = new List<SongInfoSO>();
+
+    private void Awake()
+    {
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); 
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddSong(SongInfoSO songInfo)
+    {
+        songInfos.Add(songInfo);
+    }
+
+    public SongInfoSO FindById(string id)
+    {
+        return songInfos.Find(song => song.id == id);
+    }
+}
