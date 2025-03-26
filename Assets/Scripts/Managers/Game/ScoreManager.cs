b3ca254069e6dd230bd2f4922e365b82de43cf3b
Huynh Tran Khanh Quynh <quynh190504@gmail.com>
2025-03-26 10:51:47 +0700

    Add End Game When Out Of Health

--- Assets/Scripts/Managers/Game/ScoreManager.cs
+++ Assets/Scripts/Managers/Game/ScoreManager.cs
@@ -4,16 +4,31 @@ public class ScoreManager : MonoBehaviour
 {
     private int score = 0;
     private int combo = 0;
+    private int perfect = 0;
+    private int good = 0;
+    private int bad = 0;
+    private int miss = 0;
+
+    private string rank = "NULL";
 
     [SerializeField] private ConfigSO config;
     [SerializeField] private IntPublisherSO healthEventPublisher;
     [SerializeField] private IntPublisherSO scoreEventPublisher;
     [SerializeField] private IntPublisherSO comboEventPublisher;
+    [SerializeField] private IntPublisherSO perfectEventPublisher;
+    [SerializeField] private IntPublisherSO goodEventPublisher;
+    [SerializeField] private IntPublisherSO badEventPublisher;
+    [SerializeField] private IntPublisherSO missEventPublisher;
+    [SerializeField] private StringPublisherSO rankEventPublisher;
 
     private void Start()
     {
         scoreEventPublisher.RaiseEvent(score);
         comboEventPublisher.RaiseEvent(combo);
+        perfectEventPublisher.RaiseEvent(perfect);
+        goodEventPublisher.RaiseEvent(good);
+        badEventPublisher.RaiseEvent(bad);
+        missEventPublisher.RaiseEvent(miss);
     }
 
     public void EvaluateHit(GameObject note)
@@ -25,17 +40,26 @@ public class ScoreManager : MonoBehaviour
         {
             case GameNote.HitResult.PERFECT:
                 AddScore(config.perfectScore, true);
+                perfect++;
+                perfectEventPublisher.RaiseEvent(perfect);
+                Debug.Log($"perfect {perfect}");
                 break;
             case GameNote.HitResult.GOOD:
                 AddScore(config.goodScore, true);
+                good++;
+                goodEventPublisher.RaiseEvent(good);
                 break;
             case GameNote.HitResult.BAD:
                 AddScore(config.badScore, false);
+                bad++;
+                badEventPublisher.RaiseEvent(bad);
                 ClearCombo();
                 healthEventPublisher.RaiseEvent(config.badPenalty);
                 break;
             case GameNote.HitResult.MISS:
                 ClearCombo();
+                miss++;
+                missEventPublisher.RaiseEvent(miss);
                 healthEventPublisher.RaiseEvent(config.missPenalty);
                 break;
         }
@@ -77,5 +101,22 @@ public class ScoreManager : MonoBehaviour
     {
         ClearScore();
         ClearCombo();
+        ClearNoteStats();
+    }
+
+    public void ClearNoteStats()
+    {
+        perfect = 0;
+        good = 0;
+        bad = 0;
+        miss = 0;
+    }
+
+    public void EndGame()
+    {
+        //TO DO: CALCULATE RANK
+        rank = "A";
+
+        rankEventPublisher.RaiseEvent(rank);
     }
 }
