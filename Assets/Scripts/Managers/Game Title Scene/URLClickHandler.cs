d9e02c6b0b6bf3216b70a348cbe094e85e0f5044
DanLinhHuynh-Niwashi <146794831+DanLinhHuynh-Niwashi@users.noreply.github.com>
2025-04-01 18:07:37 +0700

    Add redirection to register page

---
+++ Assets/Scripts/Managers/Game Title Scene/URLClickHandler.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class URLClickHandler : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField]
+    private string url;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Application.OpenURL(url);
+    }
+}
