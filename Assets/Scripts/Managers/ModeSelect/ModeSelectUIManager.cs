40f7457164533ca49d1a60b6699cc488b5051097
DanLinhHuynh-Niwashi <146794831+DanLinhHuynh-Niwashi@users.noreply.github.com>
2025-04-07 20:14:22 +0700

    Add song unlock

---
+++ Assets/Scripts/Managers/ModeSelect/ModeSelectUIManager.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+
+public class ModeSelectUIManager : MonoBehaviour
+{
+    public TMP_Text username;
+    public TMP_Text song_token;
+    public TMP_Text instrument_token;
+    void Start()
+    {
+        username.text = PlayerPrefs.GetString("name");
+        song_token.text = "SToken: " + SongManager.Instance.GetGameData().song_token.ToString();
+        instrument_token.text = "IToken: " + SongManager.Instance.GetGameData().instrument_token.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Managers/SongSelect/SongSelectUIManager.cs
+++ Assets/Scripts/Managers/SongSelect/SongSelectUIManager.cs
@@ -1,82 +1,172 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.Jobs;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
 
 public class SongSelectUIManager : MonoBehaviour
 {
     [Header("Song")]
     public List<SongInfoSO> songList;
     public GameObject songButtonPrefab;
-    private List<GameObject> songButtonList;
     public Transform songListContentPanel;
     public TMP_Text songNameText;
     public TMP_Text songInfoText;
+    public TMP_Text songScoreText;
+    public TMP_Text songStateText;
 
     [Header("UI")]
     public Button gameStartButton;
     public Button easyModeButton;
     public Button hardModeButton;
     public GameObject loadingPanel;
+    public GameObject unlockConfirmPanel;
     public string gameSceneName;
 
+    private List<GameObject> songButtonList = new List<GameObject>();
     private SongInfoSO currentSong;
     private GameManager.GameMode selectedGameMode;
 
-    void Start()
+    private bool? userConfirmedUnlock = null;
+
+    private void Start()
     {
         loadingPanel.SetActive(false);
+
         songList = SongManager.Instance.GetSongInfos();
-        (SongInfoSO, GameManager.GameMode) selectedSong = SongManager.Instance.GetCurrentSelectedSong();
+        var (selectedSong, savedMode) = SongManager.Instance.GetCurrentSelectedSong();
+        selectedGameMode = savedMode;
 
-        selectedGameMode = selectedSong.Item2;
+        GenerateSongButtons();
 
-        songButtonList = new List<GameObject>();
-
-        for (int i = 0; i < songList.Count; i++)
-        {
-            int index = i; // captures the current value of i to prevent all lambdas from referencing the same final value of i.
-
-            GameObject SongButton = Instantiate(songButtonPrefab, songListContentPanel);
-
-            SongButton.GetComponentInChildren<TextMeshProUGUI>().text = GetSongName(index);
-            SongButton.GetComponent<Button>().onClick.AddListener(() => SelectSong(index));
-
-            songButtonList.Add(SongButton);
-        }
         easyModeButton.onClick.AddListener(() => SetGameMode(GameManager.GameMode.NORMAL));
         hardModeButton.onClick.AddListener(() => SetGameMode(GameManager.GameMode.HARD));
 
-        if (selectedSong.Item1 == null)
+        if (selectedSong == null)
         {
-            SelectSong(0);
+            var unlockedSongs = SongManager.Instance.GetGameData().unlocked_songs;
+            string songToSelect = (unlockedSongs != null && unlockedSongs.Count > 0) ? unlockedSongs[0] : " ";
+            SelectSong(songToSelect);
         }
         else
         {
-            currentSong = selectedSong.Item1;
-            songNameText.text = currentSong.songName;
-            songInfoText.text = currentSong.info;
+            SelectSong(selectedSong.id);
         }
     }
 
-    public string GetSongName(int index)
+    private void GenerateSongButtons()
     {
-        if (index >= 0 && index < songList.Count)
-            return songList[index].songName;
-        else return "null";
+        for (int i = 0; i < songList.Count; i++)
+        {
+            int index = i; // capture index for lambda
+            var song = songList[index];
+            var songButton = Instantiate(songButtonPrefab, songListContentPanel);
+
+            songButton.GetComponentInChildren<TextMeshProUGUI>().text = song.songName;
+            songButtonList.Add(songButton);
+
+            SetupSongButton(song, songButton);
+        }
     }
 
-    public void SelectSong(int index)
+    private void SetupSongButton(SongInfoSO song, GameObject songButton)
     {
-        if (index >= 0 && index < songList.Count)
+        var data = SongManager.Instance.GetGameData();
+
+        if (SongManager.Instance.IsSongIdInData(song.id))
         {
-            currentSong = songList[index];
-            songNameText.text = currentSong.songName;
-            songInfoText.text = currentSong.info;
+            UnlockSongUI(song, songButton);
+        }
+        else if (song.isDefault)
+        {
+            data.unlocked_songs.Add(song.id);
+            UnlockSongUI(song, songButton);
+        }
+        else
+        {
+            songButton.GetComponent<Button>().onClick.AddListener(() => UnlockSong(song, songButton));
+        }
+    }
+
+    private void UnlockSongUI(SongInfoSO song, GameObject songButton)
+    {
+        songButton.transform.Find("LockPanel").gameObject.SetActive(false);
+        songButton.GetComponent<Button>().onClick.RemoveAllListeners();
+        songButton.GetComponent<Button>().onClick.AddListener(() => SelectSong(song.id));
+    }
+
+    public void UnlockSong(SongInfoSO song, GameObject songButton)
+    {
+        StartCoroutine(HandleUnlockConfirmation(song, songButton));
+    }
+
+    private IEnumerator HandleUnlockConfirmation(SongInfoSO song, GameObject songButton)
+    {
+        userConfirmedUnlock = null;
+
+        unlockConfirmPanel.SetActive(true);
+        unlockConfirmPanel.GetComponentInChildren<TextMeshProUGUI>().text =
+            $"You are unlocking {song.name} with {SongManager.Instance.GetConfig().songPrice} song tokens.";
+
+        yield return new WaitUntil(() => userConfirmedUnlock != null);
+
+        if (userConfirmedUnlock == true)
+        {
+            var data = SongManager.Instance.GetGameData();
+            int price = SongManager.Instance.GetConfig().songPrice;
+
+            if (data.song_token >= price)
+            {
+                data.song_token -= price;
+                data.unlocked_songs.Add(song.id);
+                SetupSongButton(song, songButton); // Recheck unlock status
+            }
+            else
+            {
+                Debug.LogWarning("Not enough tokens!");
+            }
+        }
+
+        unlockConfirmPanel.SetActive(false);
+    }
+
+    public void OnConfirmUnlock() => userConfirmedUnlock = true;
+    public void OnCancelUnlock() => userConfirmedUnlock = false;
+
+    public void SelectSong(string songID)
+    {
+        currentSong = SongManager.Instance.FindById(songID);
+        if (currentSong == null) return;
+
+        songNameText.text = currentSong.songName;
+        songInfoText.text = currentSong.info;
+
+        var scoreData = SongManager.Instance.GetScoreDataBySongID(currentSong.id);
+        if (scoreData)
+        {
+            if (selectedGameMode == GameManager.GameMode.NORMAL)
+            {
+                songScoreText.text = scoreData.easyScore.ToString();
+                songStateText.text = scoreData.easyState;
+            }
+            else
+            {
+                songScoreText.text = scoreData.hardScore.ToString();
+                songStateText.text = scoreData.hardState;
+            }
+        }
+        else
+        {
+            if (selectedGameMode == GameManager.GameMode.NORMAL)
+            {
+                songScoreText.text = "0";
+                songStateText.text = "";
+            }
+            else
+            {
+                songScoreText.text = "0";
+                songStateText.text = "";
+            }
         }
     }
 
@@ -88,7 +178,6 @@ public class SongSelectUIManager : MonoBehaviour
     private IEnumerator InitializeGame()
     {
         AudioManager.Instance.StopBGM();
-
         loadingPanel.SetActive(true);
 
         if (currentSong.songClip == null || currentSong.hardMidi == null || currentSong.easyMidi == null)
@@ -97,23 +186,48 @@ public class SongSelectUIManager : MonoBehaviour
             SongLoader.Instance.DownloadSong(currentSong, () => isDownloadComplete = true);
             yield return new WaitUntil(() => isDownloadComplete);
         }
-        
-        StartGame(); // Start game after download
+
+        StartGame();
     }
 
-    void StartGame()
+    private void StartGame()
     {
         SongManager.Instance.SetCurrentSelectedGame(currentSong, selectedGameMode);
         GameSceneManager.Instance.LoadScene(gameSceneName);
     }
 
-    void SetGameMode(GameManager.GameMode mode)
+    private void SetGameMode(GameManager.GameMode mode)
     {
-        this.selectedGameMode = mode;
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
+        selectedGameMode = mode;
+
+        if (currentSong == null) return;
+
+        var scoreData = SongManager.Instance.GetScoreDataBySongID(currentSong.id);
+        if (scoreData)
+        {
+            if (selectedGameMode == GameManager.GameMode.NORMAL)
+            {
+                songScoreText.text = scoreData.easyScore.ToString();
+                songStateText.text = scoreData.easyState;
+            }
+            else
+            {
+                songScoreText.text = scoreData.hardScore.ToString();
+                songStateText.text = scoreData.hardState;
+            }
+        }
+        else
+        {
+            if (selectedGameMode == GameManager.GameMode.NORMAL)
+            {
+                songScoreText.text = "0";
+                songStateText.text = "";
+            }
+            else
+            {
+                songScoreText.text = "0";
+                songStateText.text = "";
+            }
+        }
     }
 }
