244a2ab8b5fbe9d885e470689fbb8c881c815246
DanLinhHuynh-Niwashi <146794831+DanLinhHuynh-Niwashi@users.noreply.github.com>
2025-05-29 18:07:19 +0700

    Add new button animation and hover sound

--- Assets/Scripts/UIs/BaseButton.cs
+++ Assets/Scripts/UIs/BaseButton.cs
@@ -1,11 +1,42 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
+[RequireComponent(typeof(EventTrigger))]
 public class BaseButton : MonoBehaviour
 {
     [SerializeField] private VoidPublisherSO buttonClickPublisher;
     [SerializeField] private VoidPublisherSO switchSceneButtonClickPublisher;
 
+    private void Start()
+    {
+        AddPointerEnterEvent();
+    }
+
+    private void AddPointerEnterEvent()
+    {
+        EventTrigger trigger = GetComponent<EventTrigger>();
+        if (trigger == null)
+        {
+            trigger = gameObject.AddComponent<EventTrigger>();
+        }
+
+        EventTrigger.Entry entry = new EventTrigger.Entry
+        {
+            eventID = EventTriggerType.PointerEnter
+        };
+        entry.callback.AddListener((data) => { ButtonEnter(); });
+
+        trigger.triggers.Add(entry);
+    }
+
+
     public void ButtonClick()
+    {
+        if (buttonClickPublisher == null) return;
+        buttonClickPublisher.RaiseEvent();
+    }
+
+    public void ButtonEnter()
     {
         if (buttonClickPublisher == null) return;
         buttonClickPublisher.RaiseEvent();
